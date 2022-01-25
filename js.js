1647b2df7b9e611e86d33e34a97de5f3d302b564
Josip Markovic <bozadesign@gmail.com>
2022-01-25 16:31:49 +0100

    Code Refactor

--- js.js
+++ js.js
@@ -1,24 +1,17 @@
 let paragraphElement = document.getElementById("text");
-let paragraphContent = document.getElementById("text").textContent;
-let showMoreButton = document.querySelector(".show-more-btn");
-let showLessButton = document.querySelector(".show-less-btn");
+let paragraphContent = document.getElementById("text").innerText;
+let showMoreLessButton = document.querySelector(".show-more-less-btn");
 
-function shortenParagraphContent() {
-  if (paragraphContent.length > 1) {
-    paragraphElement.innerHTML = paragraphContent.slice(0, 10);
+let paragraphStatus = "shorten";
+
+paragraphElement.innerHTML = paragraphContent.substring(0, 85) + "...";
+
+const onShowMoreLessButtonClick = () => {
+  if (paragraphStatus === "shorten") {
+    paragraphElement.innerHTML = paragraphContent;
+    paragraphStatus = "normal";
+  } else if (paragraphStatus === "normal") {
+    paragraphElement.innerHTML = paragraphContent.substring(0, 85) + "...";
+    paragraphStatus = "shorten";
   }
-}
-
-const showMoreContent = () => {
-  paragraphElement.innerHTML = paragraphContent;
-  showMoreButton.style.display = "none";
-  showLessButton.style.display = "block";
 };
-
-const showLessContent = () => {
-  shortenParagraphContent();
-  showMoreButton.style.display = "block";
-  showLessButton.style.display = "none";
-};
-
-shortenParagraphContent();
